8c635042cd0bc35a8267b691cc9ad73fe06a6b11
qorlgns1 <rlgns0610@gmail.com>
2022-06-25 23:25:41 +0900

    feat: 프로젝트 초기 설정
    1. gitignore 설정
    2. prettierrc 설정
    3. styled-components 설치
    4. styled-reset 설치
    5. react-router-dom 설치
    6. global style을 위해 reset css 작성 및 적용
    7. 공통 색상 설정
    7. browserRouter를 App.jsx에 적용

---
+++ src/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  a {
+    text-decoration:none;
+    cursor: pointer;
+    color:inherit;
+  }
+    
+  * {
+    box-sizing:border-box;
+  }
+
+  body {
+    font-size: 10px;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    background-color: inherit;
+  }
+
+  input {
+    &:focus {
+      outline:none;
+    }
+  }
+
+  textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    resize: none; 
+  }
+  
+  .ir {
+    position: absolute;
+    left: -10000px;
+    top: auto;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+  }
+  
+  .a11y-hidden {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    clip-path: polygon(0 0, 0 0, 0, 0);
+  }
+
+  :root {
+    --main-color: #EA7F42;
+    --main-disabled-color: #FFC7A7;
+    --main-title-color: #000000;
+    --subtitle-text: #767676;
+    --disabled-text-color: #FFFFFF;
+    --border-gray: #DBDBDB;
+    --warning-color: #EB5757;
+    --bg-color: #FFFFFF;
+  }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>,
+);
