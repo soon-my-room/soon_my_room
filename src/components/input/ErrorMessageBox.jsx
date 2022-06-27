4996797f38e0f32e8c498a1ce3d6c4f3105092f7
qorlgns1 <rlgns0610@gmail.com>
2022-06-28 03:19:04 +0900

    feat: input box 컴포넌트와 error box 컴포넌트 개발
    컴포넌트에 font-family를 적용하기 위해 index.html에 폰트 링크 추가

---
+++ src/components/input/ErrorMessageBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ErrorMessage = styled.div`
+  margin-top: -10px 0 16px 0;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: flex-end;
+  color: var(--warning-color);
+`;
+
+export default function ErrorMessageBox({ children }) {
+  return <ErrorMessage>{children}</ErrorMessage>;
+}
