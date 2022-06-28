6f45c5e5405c7ec6109072a0452d73eba69719ac
CosmicLatte009 <ohhm009@gmail.com>
2022-06-28 20:05:15 +0900

    feat : 로그인 title 컴포넌트 개발 #10

---
+++ src/components/login/LoginTitle.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+  margin-top: 54px;
+  text-align: center;
+  color: var(--main-title-color);
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 30px;
+`;
+
+const SubTxt = styled.p`
+  margin-top: 12px;
+  text-align: center;
+  color: var(--subtitle-text);
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 14px;
+`;
+
+export default function LoginTitle({ children, subTxt }) {
+  if (subTxt === 'subTxt') {
+    return (
+      <>
+        <StyledTitle>{children}</StyledTitle>
+        <SubTxt>나중에 언제든지 변경할 수 있습니다.</SubTxt>
+      </>
+    );
+  } else return <StyledTitle>{children}</StyledTitle>;
+}
+//  프로필 설정 페이지에선 <LoginTitle subTxt='subTxt'> 로 컴포넌트 를 써줍니다.
