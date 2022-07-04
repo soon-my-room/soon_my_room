9b8675045069db5fd2c47ca23353b0167c9fa528
qorlgns1 <rlgns0610@gmail.com>
2022-07-05 02:25:17 +0900

    feat: 프로필 설정 페이지 개발
    resolve #19

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/login/Login';
+import ProfileSettingPage from './pages/ProfileSettingPage';
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/' exact component={Login} />
+        <Route path='/profile-setting' exact component={ProfileSettingPage} />
       </Switch>
     </>
   );
