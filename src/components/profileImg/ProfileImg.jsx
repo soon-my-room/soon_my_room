b687fe066661eb9ba8df2398035de6171de40adb
SeoHee3478 <seohee3478@gmail.com>
2022-07-05 21:27:24 +0900

    feat: 프로필 이미지 사진 업로드 기능 구현
    #27

--- src/components/profileImg/ProfileImg.jsx
+++ src/components/profileImg/ProfileImg.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import basicProfile from '../../assets/basic-profile.png';
 import uploadFile from '../../assets/upload-file.png';
 import styled from 'styled-components';
@@ -13,6 +13,7 @@ const ProfileImageContainer = styled.div`
 const ProfileImage = styled.img`
   width: 110px;
   height: 110px;
+  border-radius: 50%;
 `;
 
 const UploadFileImageContainer = styled.div`
@@ -35,14 +36,42 @@ const Input = styled.input`
 
 export default function ProfileImg() {
   const imageInput = useRef();
+  const [imageSrc, setImageSrc] = useState(basicProfile);
+
   const onClickImageUpload = () => {
     imageInput.current.click();
   };
+
+  const onLoadFile = async (e) => {
+    const file = e.target.files;
+    const formData = new FormData();
+    const url = 'https://mandarin.api.weniv.co.kr';
+    formData.append('image', file[0]);
+    try {
+      const response = await fetch(url + '/image/uploadfile', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      const uploadImageUrl = url + '/' + data.filename;
+      setImageSrc(uploadImageUrl);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <ProfileImageContainer>
-        <Input type='file' name='file' ref={imageInput} />
-        <ProfileImage src={basicProfile} />
+        <Input
+          type='file'
+          id='imgUpload'
+          accept='img/*'
+          onChange={onLoadFile}
+          name='file'
+          ref={imageInput}
+        />
+        <ProfileImage src={imageSrc} />
         <UploadFileImageContainer>
           <UploadFileImage src={uploadFile} onClick={onClickImageUpload} />
         </UploadFileImageContainer>
