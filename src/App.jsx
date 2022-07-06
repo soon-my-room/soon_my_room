069135d4b48989a787311e7097a675ff65fa74c2
CosmicLatte009 <ohhm009@gmail.com>
2022-07-06 16:30:29 +0900

    feat: 이메일로 회원가입 페이지 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/login/Login';
+import JoinPage from './pages/JoinPage';
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/' exact component={Login} />
+        <Route path='/join' exact component={JoinPage} />
       </Switch>
     </>
   );
