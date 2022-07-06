3b7d9a9a52bdda1c89310e3a019e82eb3f0a4610
CosmicLatte009 <ohhm009@gmail.com>
2022-07-06 16:14:56 +0900

    Revert "feat: 이메일로 회원가입 페이지 개발"
    This reverts commit 18e57b3bb901ac8d0a7e046b5d7a3238d7456f7e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/login/Login';
-import JoinPage from './pages/JoinPage';
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/' exact component={Login} />
-        <Route path='/join' exact component={JoinPage} />
       </Switch>
     </>
   );
