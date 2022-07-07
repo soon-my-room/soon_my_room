3d555786733374fdc5b82015d219ee6b1dda27a3
yeeed711 <rladpwl910@naver.com>
2022-07-07 12:12:50 +0900

    feat: 로그인 성공시 로컬스토리지에 유저 정보 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import ProfileSettingPage from './pages/ProfileSettingPage';
 import LoginHomePage from './pages/LoginHomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/' exact component={LoginHomePage} />
+        <Route path='/login' exact component={LoginPage} />
         <Route path='/profile-setting' exact component={ProfileSettingPage} />
       </Switch>
     </>
