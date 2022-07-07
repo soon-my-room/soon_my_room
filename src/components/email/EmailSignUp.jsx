b19f7736d8dffd2f60f132dee7a9546ca751744f
yeeed711 <rladpwl910@naver.com>
2022-07-07 18:52:53 +0900

    design: 이메일로 회원가입 텍스트에  display 속성 추가

--- src/components/email/EmailSignUp.jsx
+++ src/components/email/EmailSignUp.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const EmailSignUpLink = styled(Link)`
+  display: block;
   color: var(--subtitle-text);
   font-weight: 400;
   font-size: 12px;
@@ -10,7 +11,6 @@ const EmailSignUpLink = styled(Link)`
   margin-top: 20px;
 `;
 
-// 라우터는 따로 연결하지 않았습니다. 추후 페이지 제작시 email 또는 이 부분 수정해서 연결하면 됩니다.
 function EmailSignUp() {
   return <EmailSignUpLink to='email'>이메일로 회원가입</EmailSignUpLink>;
 }
