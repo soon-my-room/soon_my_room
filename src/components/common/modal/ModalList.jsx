dfdc18183ae0fe636b9f27ce00c6e6480087cc4a
CosmicLatte009 <ohhm009@gmail.com>
2022-07-11 09:25:47 +0900

    feat: 모달 컨테이너, 모달 리스트 컴포넌트 개발

---
+++ src/components/common/modal/ModalList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalLi = styled.li`
+  padding: 0 26px;
+  &:hover {
+    background-color: var(--main-color);
+  }
+`;
+const ModalBtn = styled.button`
+  width: 100%;
+  text-align: left;
+  display: block;
+  font-family: Spoqa Han Sans Neo;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 46px;
+`;
+
+export default function ModalList({ children, onClick }) {
+  return (
+    <ModalLi>
+      <ModalBtn onClick={onClick}>{children}</ModalBtn>
+    </ModalLi>
+  );
+}
