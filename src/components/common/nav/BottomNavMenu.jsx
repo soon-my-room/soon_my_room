794918a5c8239adcd6ef4ddf6cc3f3c5b4cd5920
qorlgns1 <rlgns0610@gmail.com>
2022-07-13 02:42:05 +0900

    feat: BottomNavMenu에 Item 컴포넌트로 분리 및 css 정리

--- src/components/common/nav/BottomNavMenu.jsx
+++ src/components/common/nav/BottomNavMenu.jsx
@@ -6,88 +6,64 @@ import messageIconFill from '../../../assets/icon/tab/icon-message-circle-fill.s
 import editIcon from '../../../assets/icon/tab/icon-edit.svg';
 import userIcon from '../../../assets/icon/tab/icon-user.svg';
 import userIconFill from '../../../assets/icon/tab/icon-user-fill.svg';
-import { Link } from 'react-router-dom';
+import BottomNavMenuItem from './item/BottomNavMenuItem';
 
 const MenuList = styled.ul`
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100vw;
+  height: 62px;
   display: flex;
-  flex-direction: row;
   justify-content: space-around;
-  align-items: center;
   border-top: 1px solid var(--border-gray);
-`;
-
-const MenuItem = styled.li``;
-
-const MenuBtn = styled.button`
-  margin: 14px 33px 6px 33px;
-`;
-
-const MenuTitle = styled.p`
-  color: ${(props) => props.color};
-  font-family: 'Spoqa Han Sans Neo';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 10px;
-  line-height: 14px;
-`;
-
-const MenuIcon = styled.img`
-  width: 24px;
-  height: 24px;
+  background-color: var(--bg-color);
 `;
 
 export default function BottomNavMenu(props) {
   return (
     <>
       <MenuList>
-        <MenuItem>
-          <Link to='/feed'>
-            <MenuBtn>
-              <MenuIcon src={props.type === 'feed' ? homeIconFill : homeIcon} />
-              <MenuTitle color={props.type === 'feed' ? '#EA7F42' : '#767676'}>
-                홈
-              </MenuTitle>
-            </MenuBtn>
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to='#'>
-            <MenuBtn>
-              <MenuIcon
-                src={props.type === 'chat' ? messageIconFill : messageIcon}
-              />
-              <MenuTitle color={props.type === 'chat' ? '#EA7F42' : '#767676'}>
-                채팅
-              </MenuTitle>
-            </MenuBtn>
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to='/post/upload'>
-            <MenuBtn>
-              <MenuIcon src={editIcon} />
-              <MenuTitle>게시물 작성</MenuTitle>
-            </MenuBtn>
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to='/profile'>
-            <MenuBtn>
-              <MenuIcon
-                src={props.type === 'profile' ? userIconFill : userIcon}
-              />
-              <MenuTitle
-                color={props.type === 'profile' ? '#EA7F42' : '#767676'}
-              >
-                프로필
-              </MenuTitle>
-            </MenuBtn>
-          </Link>
-        </MenuItem>
+        <BottomNavMenuItem
+          to='/feed'
+          iconSrc={props.type === 'feed' ? homeIconFill : homeIcon}
+          color={
+            props.type === 'feed' ? 'var(--main-color)' : 'var(--subtitle-text)'
+          }
+        >
+          홈
+        </BottomNavMenuItem>
+        <BottomNavMenuItem
+          to='#'
+          iconSrc={props.type === 'chat' ? messageIconFill : messageIcon}
+          color={
+            props.type === 'chat' ? 'var(--main-color)' : 'var(--subtitle-text)'
+          }
+        >
+          채팅
+        </BottomNavMenuItem>
+        <BottomNavMenuItem
+          to='/post/upload'
+          iconSrc={editIcon}
+          color={
+            props.type === 'upload'
+              ? 'var(--main-color)'
+              : 'var(--subtitle-text)'
+          }
+        >
+          게시물 작성
+        </BottomNavMenuItem>
+        <BottomNavMenuItem
+          to='/profile'
+          iconSrc={props.type === 'profile' ? userIconFill : userIcon}
+          color={
+            props.type === 'profile'
+              ? 'var(--main-color)'
+              : 'var(--subtitle-text)'
+          }
+        >
+          프로필
+        </BottomNavMenuItem>
       </MenuList>
     </>
   );
