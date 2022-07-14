cc4b57d7d79badfae6df584ba92e634759bfd8c8
yeeed711 <rladpwl910@naver.com>
2022-07-14 17:22:00 +0900

    feat: 게시글 화면에서 갤러리형/앨범형 뷰 체인지 컴포넌트 ui 개발 1. 게시글 화면에서 나타나는 갤러리형/앨범형 뷰 체인지 버튼 ui 개발했습니다.

---
+++ src/components/common/nav/PostViewChangeNav.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import postAlbumOff from '../../../assets/icon/icon-post-album-off.svg';
+import postAlbumOn from '../../../assets/icon/icon-post-album-on.svg';
+import postListOff from '../../../assets/icon/icon-post-list-on.svg';
+import postListOn from '../../../assets/icon/icon-post-list-on.svg';
+
+const Button = css`
+  width: 40px;
+  height: 40px;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const PostListView = styled.button`
+  ${Button}
+  background-image: url(${postListOn});
+`;
+
+const PostAlbumView = styled.button`
+  ${Button}
+  background-image: url(${postAlbumOff});
+`;
+
+const ToggleButtonWrap = styled.div`
+  text-align: right;
+  border-bottom: 0.5px solid var(--border-gray);
+`;
+
+export default function PostViewChangeNav() {
+  return (
+    <>
+      <ToggleButtonWrap>
+        <PostListView />
+        <PostAlbumView />
+      </ToggleButtonWrap>
+    </>
+  );
+}
