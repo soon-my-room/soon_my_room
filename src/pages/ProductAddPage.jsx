12d7fb0d2cc6194f59143ba29bcd5b183a39d496
qorlgns1 <rlgns0610@gmail.com>
2022-07-18 20:35:06 +0900

    feat: 상품등록 페이지에 이미지 등록 및 상품등록 기능 개발
    유효성검사는 아직 개발이 안되서 이미지, 상품명, 가격, 판매링크를 모두 입력하고 테스트해야합니다.

--- src/pages/ProductAddPage.jsx
+++ src/pages/ProductAddPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import TopUploadNav from '../components/common/nav/TopNavUpload';
 import ProductForm from '../components/product/ProductForm';
@@ -9,10 +10,71 @@ const ProfileEditWrap = styled.div`
 `;
 
 export default function ProductAddPage() {
+  const [storable, setStorable] = useState(false);
+  const [formInfo, setFormInfo] = useState({});
+
+  const url = 'https://mandarin.api.weniv.co.kr';
+  const saveProductImage = async () => {
+    try {
+      const path = '/image/uploadfile';
+      const response = await fetch(`${url}${path}`, {
+        method: 'POST',
+        body: formInfo.imageFormData,
+      });
+
+      const saveImageResult = await response.json();
+      return saveImageResult;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const saveProduct = async (formInfo, imagePath) => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    const { user } = userInfo;
+    const { token } = user;
+
+    try {
+      const path = '/product';
+      const response = await fetch(`${url}${path}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          product: {
+            itemName: formInfo.productName,
+            price: formInfo.productPrice,
+            link: formInfo.productLink,
+            itemImage: `${url}/${imagePath}`,
+          },
+        }),
+      });
+
+      debugger;
+      const saveImageResult = await response.json();
+      return saveImageResult;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleAddProduct = async () => {
+    const getUserImageUrl = await saveProductImage();
+    const { filename } = getUserImageUrl;
+    const message = await saveProduct(formInfo, filename);
+    console.log(message);
+  };
+
   return (
     <ProfileEditWrap>
-      <TopUploadNav buttonText='저장' buttonDisabled />
-      <ProductForm />
+      <TopUploadNav
+        buttonText='저장'
+        buttonDisabled={!storable ? 'buttonDisabled' : null}
+        onClick={handleAddProduct}
+      />
+      <ProductForm setStorable={setStorable} setFormInfo={setFormInfo} />
     </ProfileEditWrap>
   );
 }
