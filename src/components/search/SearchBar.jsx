c5bc25e9026eb26a811c02344420ca619aeb590e
SeoHee3478 <seohee3478@gmail.com>
2022-07-20 17:50:40 +0900

    feat: search bar 컴포넌트 검색 기능 구현
    1. SearchBar 컴포넌트에서 fetch 이용해서 데이터 불러오기
    
    #123

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 const SearchInput = styled.input`
@@ -17,10 +17,56 @@ const SearchInput = styled.input`
   padding-left: 16px;
 `;
 
-export default function SearchBar() {
+export default function SearchBar(props) {
+  const [keyword, setKeyword] = useState('');
+  const searchRef = useRef();
+
+  const onChangeKeyword = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  async function searchUser(token, keyword) {
+    console.log('searchUserFunction');
+    console.log('token', token);
+    console.log('keyword', keyword);
+    const url = 'https://mandarin.api.weniv.co.kr';
+    const reqPath = `/user/searchuser/?keyword=${keyword}`;
+    try {
+      const res = await fetch(url + reqPath, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      const resJson = await res.json();
+      console.log(resJson);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+  useEffect(() => {
+    if (!userInfo) {
+      console.log('로그인 정보가 없습니다.');
+      props.history.push('/login');
+      return;
+    }
+  }, []);
   return (
-    <>
-      <SearchInput type='text' placeholder='계정 검색' />
-    </>
+    <SearchInput
+      type='text'
+      placeholder='계정 검색'
+      onChange={(e) => {
+        const token = userInfo?.user?.token;
+        const searchValue = searchRef.current.value;
+
+        onChangeKeyword(e);
+        searchUser(token, searchValue);
+      }}
+      ref={searchRef}
+    />
   );
 }
