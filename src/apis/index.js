dfe85350138ebc38e596e9f6f7ee0d913a8f9669
qorlgns1 <rlgns0610@gmail.com>
2022-07-27 18:37:32 +0900

    fix: axios 이용시 토큰정보를 받아오지 못하는 이슈를 발견해서 인터셉터를 이용해서 처리

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { getUserInfo } from '../utils/userInfo';
 
-const token = getUserInfo()?.token;
-
 export const API_URL = 'https://mandarin.api.weniv.co.kr';
 
 export const axiosInstance = axios.create({
@@ -16,15 +14,17 @@ export const axiosInstanceWithToken = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
   },
 });
 
 axiosInstanceWithToken.interceptors.request.use((request) => {
   // axiosInstanceWithToken에서 토큰이 존재하지 않으면 서버에 request를 보내지 않습니다.
+  const { token } = getUserInfo();
   if (!token) {
     throw new Error('토큰이 없습니다. 다시 로그인해주세요.');
   }
 
+  // 토큰이 존재하면 headers에 토큰을 넣어서 서버에 request를 보냅니다.
+  request.headers.Authorization = `Bearer ${token}`;
   return request;
 });
